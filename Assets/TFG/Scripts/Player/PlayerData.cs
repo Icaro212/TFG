cbe959ff1a02152339ace0833ee15af0bb076877
Pablo José <pablorcmail@gmail.com>
2023-10-27 21:47:28 +0200

    Feat: Building Tutorial Scene

---
+++ Assets/TFG/Scripts/Player/PlayerData.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+[CreateAssetMenu(menuName ="Player Data")]
+public class PlayerData : ScriptableObject
+{
+    [Header("General")]
+    public float movementSpeed;
+    public float maxSpeed;
+    public float maxFallSpeed;
+    public float runAccelAmount;
+    public float runDeccelAmount;
+    public float gravityScale;
+    public float fallGravityMult;
+    public float jumpHangGravityMul;
+
+    [Space(5)]
+    [Header("Jump")]
+    public float jumpForce = 14.0f;
+    public float groundCheckRadius;
+    public float accelInAir;
+    public float deccelInAir;
+
+    [Space(5)]
+    [Header("WallSlide")]
+    public float wallCheckDistance;
+    public float wallSlideSpeed;
+    public float wallSlideAccel;
+
+    [Space(5)]
+    [Header("WallJump")]
+    public float wallJumpDirection = 0;
+    public Vector2 wallJumpVector;
+
+    [Space(5)]
+    [Header("Assits Parameters")]
+    [Range(0.01f, 0.5f)] public float coyoteTime = 0.15f; // Grace period after falling off a platform, where you can still jump
+    public float LastOnGroundTime;
+
+    [Space(5)]
+    [Header("Animation and Layer Mask")]
+    public Animator anim;
+    public LayerMask layerMask;
+
+
+
+}
